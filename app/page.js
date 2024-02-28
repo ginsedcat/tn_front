86d5df10c83ab250edcd496a696ba6654dddc9b4
Your Name <you@example.com>
2024-02-28 19:01:26 +0200

    remoded unused

--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
 "use client";
 
 // import styles from "./page.module.css";
-import { Register } from './comp/register';
 
 export default function Home() {
   return (
-    <Register></Register>
+    <></>
   );
 }
